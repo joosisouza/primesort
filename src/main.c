a072477230542021d778db3c7fca3886b62476b6
Josiane Souza <josi.canobel@gmail.com>
2017-11-06 20:35:47 -0200

    Programa para ordenar um vetor de números não primos

--- src/main.c
+++ src/main.c
@@ -7,11 +7,49 @@
 
 #include <stdio.h>
 
+int ehPrimo(int n){
+    int i;
+    int div = 0;
+
+    //Caso especial para n=1, que não é primo
+    if (n == 1){
+        return 0;
+    }
+
+    //Checa se o número é par (com excessão do 2, que é primo)
+    if ((n != 2) && (n%2 == 0)) {
+        return 0;
+    }
+
+    // Checa se existe algum outro divisor
+    for(i=3;i<n;i+=2){
+        if(n%i==0){
+            return 0;
+        }
+    }
+    return 1;
+}
+
 int main() {
-
-  int x, y;
-
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
-  return 0;
+    int leitura;
+    FILE *arquivo = fopen("arquivo.txt","w");
+    while(1){
+        scanf("%d",&leitura);
+        if(leitura>=0){
+            if((ehPrimo(leitura)==0) && (leitura != 0)){
+                fprintf(arquivo,"%d\n",leitura);
+            }
+        }
+        else{
+            fclose(arquivo);
+            break;
+        }
+    }
+    FILE *fp = popen("sort -n -r arquivo.txt","r");
+    char ler[30];
+    while(fgets(ler,30,fp)!=NULL){
+        printf("%s",ler);
+    }
+    fclose(fp);
+    return 0;
 }
